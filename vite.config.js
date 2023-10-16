051ecaab948adb6068a7b7e587432b80e471fa48
Nicolas Towery <nicolastowery@gmail.com>
2023-10-16 14:18:16 -0500

    edits for gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/drone-customer-portal-client/",
   plugins: [react(), eslint()],
-  base: "/vite-deploy/"
 });
